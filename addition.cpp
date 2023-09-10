c0659d2b9c30386cb2a6d1172135a7f692f19ce9
未来 <101379299+Believe0127@users.noreply.github.com>
2023-09-10 12:14:26 +0900

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void calculate(){
+    int value1;
+    int value2;
+
+    cin >> value1;
+    cout << "+\n";
+    cin >> value2;
+    cout << "ŒvŽZŒ‹‰Ê: " << value1 + value2 << endl;
+}
+
+int main(){
+    cout << "2‚Â‚Ì”’l‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F\n";
+
+    calculate();
+    return 0;
+
+}
